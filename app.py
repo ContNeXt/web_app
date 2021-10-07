28774bb9fcd63bd39327187322f93c0954f84dd8
sara.diaz.del.ser <sara.diaz.delser@scai.franhofer.de>
2021-10-07 23:50:16 +0200

    structure
    Signed-off-by: sara.diaz.del.ser <sara.diaz.delser@scai.franhofer.de>

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+# from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = "changethislater"
+# app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
@@ -10,17 +13,42 @@ def home():
 def page():
    return render_template("page.html")
 
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        # session.permanent = True
+        user = request.form["nm"]
+        session["user"] = user
+        flash(f"You have logged in successfully, {user}", "info")
+
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+        return render_template("login.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in!", "info")
+        return redirect(url_for("login"))
+
+@app.route("/logout")
+def logout():
+    if "user" in session:
+        user = session["user"]
+        session.pop("user", None)
+        flash(f"You have been logged out, {user}", "info")
+    return redirect(url_for("login"))
+
+
 @app.route("/admin")
 def admin():
    return render_template("admin.html")
-
-#@app.route("/<name>")
-#def user(name):
-#    return f'Hello {name}!'
-
-#@app.route("/admin")
-#def admin():
-#    return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
     app.run(debug=True)
