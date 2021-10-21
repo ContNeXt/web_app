6c2701c14c334f13ffecdc620a7901289331cc03
sara.diaz.del.ser <sara.diaz.delser@scai.franhofer.de>
2021-10-21 22:51:15 +0200

    database
    Signed-off-by: sara.diaz.del.ser <sara.diaz.delser@scai.franhofer.de>

--- src/load_db.py
+++ src/load_db.py
@@ -6,9 +6,9 @@ RUN ONCE TO CREATE THE DATABASE
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
-#from models import Networks, Nodes
+import networkx as nx
+# from models import Networks, Nodes
 
-import csv
 from tqdm import tqdm
 import os
 
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 # SQLAlchemy
 db_name = "testDatabase.db"
-data_source ="networks2"
+data_source ="networks"
 
 app.config['SECRET_KEY'] = "1P313P4OO138O4UQRP9343P4AQEKRFLKEQRAS230"
 
@@ -27,6 +27,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database
 db = SQLAlchemy(app)
+
 
 '''
 database structure 
@@ -47,7 +48,6 @@ class Networks(db.Model):
     __tablename__ = 'networks'
     id = db.Column(db.Integer, primary_key=True)
     # net_id= db.Column(db.Integer, db.ForeignKey('nodes.id'), nullable=False)
-    # !!!! need to make node_id a list.
     geneStart = db.Column(db.String(20), nullable=False)
     geneEnd = db.Column(db.String(20), nullable=False)
     direction = db.Column(db.String(20), nullable=True, default='0')
@@ -62,32 +62,6 @@ class Networks(db.Model):
         self.weight = weight
 
 
-'''
-Function loads data from given tsv file into database1.db
-!!!!! ATTENTION: currently set to csv, instead !!!!!
-'''
-
-def load_database(tsv_file):
-    # Open tsv file
-    with open(tsv_file) as f:
-        reader = csv.reader(f, delimiter=';')
-        # skip header
-        next(reader, None)
-        for row in reader:
-            if row:
-                newNET = Networks(geneStart=row[0],
-                              geneEnd=row[1],
-                              direction=row[2],
-                              method=row[3],
-                              weight=row[4],
-                              )
-                NOD1 = Nodes(name=row[0])
-                NOD2 = Nodes(name=row[1])
-                # Push to Database
-                db.session.add(newNET, NOD2, NOD1)
-                db.session.commit()
-    source = tsv_file
-    return source
 
 '''
 For given path, find all tsv files
@@ -95,51 +69,64 @@ For given path, find all tsv files
 
 def list_files(dir):
     # create a list of file and sub directories
-    listOfFile = os.listdir(dir)
-    allFiles = list()
+    list_of_files = os.listdir(dir)
+    all_files = list()
     # Iterate over all the entries
-    for entry in listOfFile:
+    for entry in list_of_files:
         # Create full path
-        fullPath = os.path.join(dir, entry)
+        full_path = os.path.join(dir, entry)
         # If entry is a directory then get the list of files in this directory
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + list_files(fullPath)
+        if os.path.isdir(full_path):
+            allFiles = all_files + list_files(full_path)
         else:
-            allFiles.append(fullPath)
-    return allFiles
+            all_files.append(full_path)
+    return all_files
 
-def check_tsv(allFiles):
+def check_tsv(all_files):
     list_tsv = list()
-    for each in allFiles:
+    for each in all_files:
         # Check the extension
-        if each.endswith(".csv"):
+        if each.endswith(".tsv"):
             list_tsv.append(each)
     return list_tsv
 
+def add_edgelist(file_path):
+    # Open the file as an nx object
+    network = nx.read_edgelist(file_path, comments='from', delimiter='\t', data=(
+        ("direction", str),
+        ("method", str),
+        ("weight", float),), encoding='utf-8')
+    return network
 
 '''
     Run app
 '''
 
 @app.route('/')
-def testdb():
-    # Load data
+def load_database(data_source="/Users/sdiazdelser/Downloads/networks"):
     # Find all files in directory
-    allFiles = list_files(data_source)
-    # filter out all non-tsv
-    list_tsv= check_tsv(allFiles)
+    all_files = list_files(data_source)
 
-    # add data from each file to database
+    # filter out all non-tsv
+    list_tsv= check_tsv(all_files)
+
     sources = list()
+    # add data from each file to database
     try:
         for each in tqdm(list_tsv):
-            source = load_database(each)
-            sources.append(source)
+            network = add_edgelist(each)
+            new_node = Nodes(list(network.nodes))
+            new_network = Networks(list(network.edges))
+
+            # to make sure it's working
+            sources.append(network)
+            #db.session.add(new_node)
+            #db.session.add(new_network)
+            #db.session.commit()
         return f"<h1> Success! Database created from: { str(sources) } </h1>"
 
     except:
         return "<h1>Something went wrong! Database was not created correctly.</h1>"
-
 
 
 if __name__ == "__main__":
