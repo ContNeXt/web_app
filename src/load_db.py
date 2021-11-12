c64d4f70f9daedcc5298ce4bc7ed89d383d3003d
sara diaz <sara.diaz.del.ser@scai.fraunhofer.de>
2021-11-12 12:55:23 +0100

    add json

--- src/load_db.py
+++ src/load_db.py
@@ -11,13 +11,14 @@ from models import Network, Node
 from tqdm import tqdm
 import os
 import sys
+import json
 
 app = Flask(__name__)
 
 # SQLAlchemy
 db_name = "database.db"
-#data_source = sys.argv[1]
-data_source= "/Users/sara/Downloads/networks2"
+data_source = sys.argv[1]
+#data_source= "/Users/sara/Downloads/networks2"
 
 app.config['SECRET_KEY'] = "1P313P4OO138O4UQRP9343P4AQEKRFLKEQRAS230"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_name
@@ -149,6 +150,8 @@ def load_database(data_source=data_source):
 
     # counter
     debugger = int(0)
+    # init list of nodes
+    list_of_nodes = []
 
     # add data from each file to database
     for key, value in tqdm(list_tsv.items()):
@@ -161,6 +164,7 @@ def load_database(data_source=data_source):
 
         for node in network.nodes:
             new_node = Node(name=node, context='tissues')
+            list_of_nodes.append(node)
 
             # add relationship between nodes
             new_network._nodes.append(new_node)
@@ -171,6 +175,10 @@ def load_database(data_source=data_source):
         # push to database
         db.session.add(new_network)
         db.session.commit()
+
+        # create json file with list of nodes
+        with open ("static/json/nodes.json", 'w') as f:
+            json.dump(list_of_nodes, f)
 
     return f"<h1> Files uploaded to database: {str(debugger) } </h1>"
 
