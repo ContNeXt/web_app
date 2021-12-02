29cff68b5cfb9abf3c05e79585a9b2e22d9f5378
sara diaz <sara.diaz.del.ser@scai.fraunhofer.de>
2021-12-02 11:28:14 +0100

    css fix

---
+++ src/static/js/graph.js
@@ -0,0 +1,13 @@
+// graph.js
+
+// C3 graphs
+// automatically does a line-chart
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+});
