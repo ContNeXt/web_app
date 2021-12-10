09b75572deace636f0987be822c8d5d526ed4f04
sara diaz <sara.diaz.del.ser@scai.fraunhofer.de>
2021-12-10 11:32:05 +0100

    cli

--- cli.py
+++ cli.py
@@ -1,1 +1,14 @@
-#TODO change name to Bt_css
+import os.path
+import click
+
+DATABASE_LOADER = os.path.abspath('load_db.py')
+
+@click.group()
+def main():
+	"""Entry method."""
+
+@main.command()
+@click.argument('source-dir')
+def compile(source_dir: str):
+	"""Creates a database from source directory."""
+	os.system(f"DATABASE_LOADER {source_dir}")
