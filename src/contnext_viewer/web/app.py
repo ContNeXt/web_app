6e40557663c2df26868881dba2d9b817bcb5db2c
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2022-05-20 16:48:09 +0200

    Apply pycharm formatting

--- src/contnext_viewer/web/app.py
+++ src/contnext_viewer/web/app.py
@@ -5,6 +5,7 @@
 import logging
 import time
 
+
 from flask import Flask
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -18,36 +19,36 @@ log = logging.getLogger(__name__)
 
 
 def create_app(template_folder: str = None, static_folder: str = None):
-	"""Create the Flask application"""
-	t = time.time()
+    """Create the Flask application"""
+    t = time.time()
 
-	app = Flask(__name__,
-				template_folder=(template_folder or '../templates'),
-				static_folder=(static_folder or '../static')
-				)
+    app = Flask(__name__,
+                template_folder=(template_folder or '../templates'),
+                static_folder=(static_folder or '../static')
+                )
 
-	cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-	app.config['CORS_HEADERS'] = 'Content-Type'
+    cors = CORS(app, resources={r"/foo": {"origins": "*"}})
+    app.config['CORS_HEADERS'] = 'Content-Type'
 
-	app.config['SECRET_KEY'] = "1P313P4OO138O4UQRP9343P4AQEKRFLKEQRAS230"
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_PATH + '?check_same_thread=False'
-	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = "1P313P4OO138O4UQRP9343P4AQEKRFLKEQRAS230"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + DB_PATH + '?check_same_thread=False'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-	# Initialize the database
-	db = SQLAlchemy(app)
+    # Initialize the database
+    db = SQLAlchemy(app)
 
-	# Add Admin view
-	admin = Admin(app)
-	admin.add_view(ModelView(Network, db.session))
-	admin.add_view(ModelView(Node, db.session))
+    # Add Admin view
+    admin = Admin(app)
+    admin.add_view(ModelView(Network, db.session))
+    admin.add_view(ModelView(Node, db.session))
 
-	'''
-		Blueprints for the website
-	'''
-	app.register_blueprint(contnext)
+    '''
+        Blueprints for the website
+    '''
+    app.register_blueprint(contnext)
 
-	log.info('Done building %s in %.2f seconds', app, time.time() - t)
-	return app
+    log.info('Done building %s in %.2f seconds', app, time.time() - t)
+    return app
 
 
 '''
@@ -55,4 +56,4 @@ def create_app(template_folder: str = None, static_folder: str = None):
 '''
 app = create_app()
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
--- src/contnext_viewer/web/views.py
+++ src/contnext_viewer/web/views.py
@@ -4,26 +4,28 @@
 import logging
 import re
 
+
+from flask import jsonify, Blueprint, render_template, request, redirect, url_for, session
+from flask_cors import cross_origin
+from pkg_resources import resource_filename
+from sqlalchemy.orm import sessionmaker, scoped_session
+
 from contnext_viewer.constants import CONTEXT
 from contnext_viewer.graph import create_json_file
 from contnext_viewer.models import Network, Node, engine
 from contnext_viewer.web.autocomplete import autocomplete_search
-from flask import jsonify, Blueprint, render_template, request, redirect, url_for, session
-from flask_cors import cross_origin
-from pkg_resources import resource_filename
-from sqlalchemy import and_
-from sqlalchemy.orm import sessionmaker, scoped_session
 
 log = logging.getLogger(__name__)
 
 contnext = Blueprint(
-	'contnext_viewer',
-	__name__,
-	template_folder=resource_filename('contnext_viewer', 'templates'),
-	static_folder=resource_filename('contnext_viewer', 'templates')
+    'contnext_viewer',
+    __name__,
+    template_folder=resource_filename('contnext_viewer', 'templates'),
+    static_folder=resource_filename('contnext_viewer', 'templates')
 )
 
 sqlsession = scoped_session(sessionmaker(bind=engine))
+
 
 @contnext.teardown_request
 def remove_session(ex=None):
@@ -32,157 +34,157 @@ def remove_session(ex=None):
 
 @contnext.route("/")
 def main():
-	"""Redirect to ContNeXt home page."""
-	return redirect(url_for("contnext_viewer.home"))
+    """Redirect to ContNeXt home page."""
+    return redirect(url_for("contnext_viewer.home"))
 
 
 @contnext.route("/home", methods=['GET', 'POST'])
 def home():
-	"""Search function in Home"""
-	if request.method == 'POST':
-		try:
-			if request.form.get('node-query') and request.form.get('queryOptions'):
-				# Fetch query data
-				session['node-query'] = request.form['node-query']
-				session['idoptions'] = request.form['queryOptions']
-				session['form'] = 'node'
-				return redirect(url_for("contnext_viewer.query", query=session['node-query']))
+    """Search function in Home"""
+    if request.method == 'POST':
+        try:
+            if request.form.get('node-query') and request.form.get('queryOptions'):
+                # Fetch query data
+                session['node-query'] = request.form['node-query']
+                session['idoptions'] = request.form['queryOptions']
+                session['form'] = 'node'
+                return redirect(url_for("contnext_viewer.query", query=session['node-query']))
 
-			elif request.form.get('network-query'):
-				# Fetch query data
-				session['network-query'] = request.form['network-query']
-				session['form'] = 'network'
-				# check if query is an id or a network name
-				if re.match('(UBERON|CL|CLO):\d+', session['network-query']):
-					session['query-param'] = 'identifier'
-				else:
-					session['query-param'] = 'name'
-				return redirect(url_for("contnext_viewer.query", query=session['network-query']))
-		except:
-			return render_template("error500.html")
-	else:
-		return render_template("home.html")
+            elif request.form.get('network-query'):
+                # Fetch query data
+                session['network-query'] = request.form['network-query']
+                session['form'] = 'network'
+                # check if query is an id or a network name
+                if re.match('(UBERON|CL|CLO):\d+', session['network-query']):
+                    session['query-param'] = 'identifier'
+                else:
+                    session['query-param'] = 'name'
+                return redirect(url_for("contnext_viewer.query", query=session['network-query']))
+        except:
+            return render_template("error500.html")
+    else:
+        return render_template("home.html")
 
 
 @contnext.route("/query/<query>")
 def query(query):
-	"""Generate results according to query"""
-	# Import query parameters
-	context = session.get('idoptions', None)
-	param = session.get('query-param', None)
-	form = session.get('form', None)
+    """Generate results according to query"""
+    # Import query parameters
+    context = session.get('idoptions', None)
+    param = session.get('query-param', None)
+    form = session.get('form', None)
 
-	# Start database session
-	try:
-		if form == 'node':
-			# Get list of all the ids for that node
-			node_ids = [each.id for each in sqlsession.query(Node).filter(Node.name == query).all()]
+    # Start database session
+    try:
+        if form == 'node':
+            # Get list of all the ids for that node
+            node_ids = [each.id for each in sqlsession.query(Node).filter(Node.name == query).all()]
 
-			# For each id, get list of all networks associated with it.
-			list_of_nodes = {}
+            # For each id, get list of all networks associated with it.
+            list_of_nodes = {}
 
-			qry = sqlsession.query(Node).filter(Node.id.in_(node_ids)	
-			print(qry)
-							
-			for node in qry.all():
-				for network in node.networks_:
-					if network.context != context:
-						continue
-							    
-					list_of_nodes.update({network.identifier: [network.data, network.name, network.properties]})
-				
-			return render_template("results.html", idquery=query, idoptions=context, form=form, results=list_of_nodes)
+            qry = sqlsession.query(Node).filter(Node.id.in_(node_ids))
+            print(qry)
+            for node in qry.all():
 
-		elif form == 'network' and param == 'identifier':
-			# Get network info
-			list_of_networks = [[network.identifier, network.data, network.name, network.properties, network.context]
-								for network in sqlsession.query(Network).filter(Network.identifier == query).all()][0]
-			return render_template("results.html", idquery=query, idoptions=context, form=form,
-								   results=list_of_networks)
+                for network in node.networks_:
+                    if network.context != context:
+                        continue
 
-		elif form == 'network' and param == 'name':
-			# Get network info
-			list_of_networks = [[network.identifier, network.data, network.name, network.properties, network.context]
-								for network in sqlsession.query(Network).filter(Network.name == query).all()][0]
+                    list_of_nodes.update({network.identifier: [network.data, network.name, network.properties]})
 
-			return render_template("results.html", idquery=query, idoptions=context, form=form,
-								   results=list_of_networks)
-	except IndexError:
-		return render_template("error500.html")
+            return render_template("results.html", idquery=query, idoptions=context, form=form, results=list_of_nodes)
+
+        elif form == 'network' and param == 'identifier':
+            # Get network info
+            list_of_networks = [[network.identifier, network.data, network.name, network.properties, network.context]
+                                for network in sqlsession.query(Network).filter(Network.identifier == query).all()][0]
+            return render_template("results.html", idquery=query, idoptions=context, form=form,
+                                   results=list_of_networks)
+
+        elif form == 'network' and param == 'name':
+            # Get network info
+            list_of_networks = [[network.identifier, network.data, network.name, network.properties, network.context]
+                                for network in sqlsession.query(Network).filter(Network.name == query).all()][0]
+
+            return render_template("results.html", idquery=query, idoptions=context, form=form,
+                                   results=list_of_networks)
+    except IndexError:
+        return render_template("error500.html")
 
 
 @contnext.route("/heatmap/<context>")
 def heatmap(context):
-	if CONTEXT.get(context):
-		title = CONTEXT.get(context)
-		if context == 'cell_line' or context == 'cellline':
-			json_path = "/static/json/cellline-pathway_clustergrammer.json"
-		elif context == 'cell_type' or context == 'celltype':
-			json_path = "/static/json/celltype-pathway_clustergrammer.json"
-		elif context == 'tissue' or context == 'tissue':
-			json_path = "/static/json/tissue-pathway_clustergrammer.json"
-		return render_template("heatmaps.html", title=title, json_path=json_path)
-	else:
-		return render_template("error404.html")
+    if CONTEXT.get(context):
+        title = CONTEXT.get(context)
+        if context == 'cell_line' or context == 'cellline':
+            json_path = "/static/json/cellline-pathway_clustergrammer.json"
+        elif context == 'cell_type' or context == 'celltype':
+            json_path = "/static/json/celltype-pathway_clustergrammer.json"
+        elif context == 'tissue' or context == 'tissue':
+            json_path = "/static/json/tissue-pathway_clustergrammer.json"
+        return render_template("heatmaps.html", title=title, json_path=json_path)
+    else:
+        return render_template("error404.html")
 
 
 @contnext.route("/about")
 def about():
-	return render_template("about.html")
+    return render_template("about.html")
 
 
 @contnext.route("/imprint")
 def imprint():
-	return render_template("imprint.html")
+    return render_template("imprint.html")
 
 
 @contnext.route("/tutorial")
 def tutorial():
-	return render_template("tutorial.html")
+    return render_template("tutorial.html")
 
 
 @contnext.route("/admin")
 def admin():
-	return render_template("admin.html")
+    return render_template("admin.html")
 
 
 @contnext.route("/graph/<node>/<network_id>")
 def graph(node, network_id):
-	nodes, links = create_json_file(id=network_id, node=node)
-	network = {'nodes': nodes, 'links': links}
-	return render_template("explorer.html", network_id=network_id, node=node, network_json=network)
+    nodes, links = create_json_file(id=network_id, node=node)
+    network = {'nodes': nodes, 'links': links}
+    return render_template("explorer.html", network_id=network_id, node=node, network_json=network)
 
 
 # autocomplete API: node list json
 @contnext.route("/api/autocomplete", methods=['POST'])
 @cross_origin(origin='*', headers=['Content-Type', 'Authorization'], support_credentials=True)
 def node_autocompletion():
-	q = request.form['q']
-	resource = request.form['resource']
-	if not q or not resource:
-		return jsonify({})
+    q = request.form['q']
+    resource = request.form['resource']
+    if not q or not resource:
+        return jsonify({})
 
-	results = autocomplete_search(query=q, context=resource, limit=10)
-	if not results:
-		return jsonify({})
+    results = autocomplete_search(query=q, context=resource, limit=10)
+    if not results:
+        return jsonify({})
 
-	return jsonify(results)
+    return jsonify(results)
 
 
 # network explorer API: result json
 @contnext.route("/api/neighbouring-nodes/<node>/<network_id>", methods=['GET'])
 def network_explorer(node, network_id):
-	if (request.method == 'GET'):
-		nodes, links = create_json_file(id=network_id, node=node)
-		return jsonify({'nodes': nodes, 'links': links})
+    if (request.method == 'GET'):
+        nodes, links = create_json_file(id=network_id, node=node)
+        return jsonify({'nodes': nodes, 'links': links})
 
 
 # Error handling
 @contnext.errorhandler(404)
 def not_found():
-	return render_template("error404.html")
+    return render_template("error404.html")
 
 
 @contnext.errorhandler(500)
 def not_found():
-	return render_template("error404.html")
+    return render_template("error404.html")
